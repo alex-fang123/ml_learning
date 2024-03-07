2d1cfe0156adc5b704f0a7329c87e3255bba7999
xz_fa <fangxz2002@outlook.com>
2024-03-07 22:54:40 +0800

    添加后剪枝部分

--- 判断好瓜2 决策树 ID3/main.py
+++ 判断好瓜2 决策树 ID3/main.py
@@ -25,3 +25,4 @@ test = data.iloc[[3, 4, 7, 8, 10, 11, 12],:]
 # 这里训练出来的结果和西瓜书P81的结果不一样，
 # 这是因为在第一次选择最优划分属性时，色泽和脐部属性在测试集上的正确率是一样的，我的模型用的色泽，书上用的脐部
 my_tree1 = fc.GenerateTree(data, "好瓜", ["密度", "含糖率"], "pre", train, test)
+my_tree2 = fc.GenerateTree(data, '好瓜', [], pruning='post', train=train, test=test)
