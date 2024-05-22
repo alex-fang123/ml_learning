f40025a7e0d0c9e1f1ca1a283201b27eafa32fe1
xz_fa <fangxz2002@outlook.com>
2024-05-22 12:51:56 +0800

    numpy实现BP

--- BP算法 heart disease/main.py
+++ BP算法 heart disease/main.py
@@ -5,19 +5,24 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 
 class LinearLayer:
     """
     定义好线性层就可以搭积木了
     """
+
     def __init__(self, input_size, output_size):
         self.weights = np.random.randn(input_size, output_size)
         self.threshold = np.random.randn(output_size, 1)
+        print(self.threshold)
         self.input = None
         self.output = None
-        self.delta = None
-        self.delta_weights = None
-        self.delta_bias = None
+        self.delta = np.zeros(np.shape(self.threshold))
+        self.delta_weights = np.zeros(np.shape(self.weights))
+        self.learning_rate = 0.001
 
     def forward(self, input):
         self.input = input
@@ -31,12 +36,18 @@ class LinearLayer:
         :param dout: 上一层的输出求偏导的结果
         :return:
         """
-        self.delta = dout * self.output * (1 - self.output)
-        self.delta_weights = np.dot(self.input, self.delta.T)
+        self.delta += dout * self.output * (1 - self.output)
+        self.delta_weights += self.input * self.delta.T
         dx = np.dot(self.weights, self.delta)
-        self.weights += self.delta_weights
-        self.threshold += -self.delta
         return dx
+
+    def update(self, learning_rate):
+        self.weights += self.delta_weights * learning_rate
+        self.threshold += self.delta * learning_rate
+        self.delta = np.zeros(np.shape(self.threshold))
+        self.delta_weights = np.zeros(np.shape(self.weights))
+        return self.weights, self.threshold
+
 
 class MLP:
     def __init__(self, input_size, hidden_size, output_size):
@@ -49,13 +60,67 @@ class MLP:
     def backward(self, dout):
         return self.linear1.backward(self.linear2.backward(dout))
 
+    def update(self):
+        self.linear1.update(self.linear1.learning_rate)
+        self.linear2.update(self.linear2.learning_rate)
+
 
 data = pd.read_csv('./archive/heart_statlog_cleveland_hungary_final.csv')
-label = data['target']
-features = data.drop(columns=['target'])
+x_train, x_test, y_train, y_test = train_test_split(data.drop(columns=['target']), data['target'], test_size=0.2)
 
-features = preprocessing.StandardScaler().fit_transform(features)
-input_size = features.shape[1]
-hidden_size = 10
+x_train.reset_index(drop=True, inplace=True)
+x_test.reset_index(drop=True, inplace=True)
+y_train.reset_index(drop=True, inplace=True)
+y_test.reset_index(drop=True, inplace=True)
+
+x_train = preprocessing.StandardScaler().fit_transform(x_train)
+x_test = preprocessing.StandardScaler().fit_transform(x_test)
+
+input_size = x_train.shape[1]
+hidden_size = 20
 output_size = 1
-batch_size = 1
+batch_size = 32
+
+num_epochs = 100
+mlp = MLP(input_size, hidden_size, output_size)
+losses_train = []
+losses_test = []
+
+single_loss = 0
+for epoch in range(num_epochs):
+    if epoch % 10 == 0:
+        print(f'Epoch {epoch}')
+    for j in range(np.shape(y_train)[0] // batch_size + 1):
+        if j != np.shape(y_train)[0] // batch_size:
+            for i in range(j * batch_size, (j + 1) * batch_size):
+                temp = x_train[i].reshape(-1, 1)
+                mlp.forward(temp)
+                single_loss += np.sum((y_train[i] - mlp.linear2.output) ** 2)
+                mlp.backward(y_train[i] - mlp.linear2.output)
+            mlp.update()
+            losses_train.append(single_loss)
+            single_loss = 0
+        else:
+            for i in range(j * batch_size, np.shape(y_train)[0]):
+                temp = x_train[i].reshape(-1, 1)
+                mlp.forward(temp)
+                single_loss += np.sum((y_train[i] - mlp.linear2.output) ** 2)
+                mlp.backward(y_train[i] - mlp.linear2.output)
+            mlp.update()
+            losses_train.append(single_loss)
+            single_loss = 0
+
+        # 在测试集上测试
+        test_output = pd.DataFrame(x_test).apply(lambda x: mlp.forward(x.values.reshape(-1, 1))[0][0], axis=1)
+        losses_test.append(np.sum((y_test - test_output) ** 2))
+
+# 画图
+dev_x = [i * 10 for i in range(len(losses_train))]
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+line1 = ax1.plot(dev_x, losses_train, 'g-', label='train loss')
+line2 = ax2.plot(dev_x, losses_test, 'r-', label='test loss')
+ax1.set_xlabel('step count')
+ax1.set_ylabel('train loss', color='g')
+ax2.set_ylabel('test loss', color='r')
+plt.show()
