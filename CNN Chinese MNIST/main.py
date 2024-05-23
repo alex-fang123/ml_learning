93e34172b1643a8ec0210d62edec660b13ce9b23
xz_fa <fangxzh@pku.edu.cn>
2024-05-23 14:47:37 +0800

    添加maxpool的前馈

--- CNN Chinese MNIST/main.py
+++ CNN Chinese MNIST/main.py
@@ -16,8 +16,8 @@ def get_tar_jpg(id1, id2, id3):
     return img
 
 
-# %% convolution
-class filter:
+# %% convolution object
+class Filter:
     def __init__(self, width, height, depth, stride, if_paddle, learning_rate=0.01):
         self.width = width
         self.height = height
@@ -25,7 +25,7 @@ class filter:
         self.stride = stride
         self.if_paddle = if_paddle  # 这里设计的补0主要是针对stride大于1时带来边缘信息没有卷积到的情况，参考forward函数的第一个if
         self.weights = np.random.randn(height, width)
-        self.bias = np.random.randn(1)
+        # self.bias = np.random.randn(1)
         self.learning_rate = learning_rate
 
     def forward(self, picture):
@@ -45,20 +45,83 @@ class filter:
         feature_map = np.zeros((out_height, out_width))
         for i in range(out_height):
             for j in range(out_width):
-                feature_map[i, j] = np.sum(picture[i:i + self.height, j:j + self.width] * self.weights) + self.bias
+                # relu
+                feature_map[i, j] = max(np.sum(picture[i:i + self.height, j:j + self.width] * self.weights), 0)
         return feature_map
 
 
-class softmax:
-    def __init__(self, input_size, output_size, learning_rate=0.01):
-        self.input_size = input_size
-        self.output_size = output_size
-        self.weights = np.random.randn(input_size, output_size)
-        self.bias = np.random.randn(1)
-        self.learning_rate = learning_rate
+class ConvolutionLayer:
+    def __int__(self):
+        self.conv1 = Filter(3, 3, 3, 1, 1)
+        self.conv2 = Filter(3, 3, 3, 1, 1)
 
-    def forward(self, x):
-        return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+class Softmax:
+    def __init__(self, image):
+        self.image = image
+
+    def forward(self):
+        return np.exp(self.image) / np.sum(np.exp(self.image), axis=0)
+
+
+class MaxPool:
+    def __init__(self, image, pool_size, stride):
+        self.image = image
+        self.height, self.width, self.depth = image.shape
+        self.pool_size = pool_size
+        self.stride = stride
+
+    def forward(self):
+        out_height = (self.height - self.pool_size) // self.stride + 1
+        out_width = (self.width - self.pool_size) // self.stride + 1
+        out = np.zeros((out_height, out_width, self.depth))
+        for d in range(self.depth):
+            for i in range(out_height):
+                for j in range(out_width):
+                    out[i, j] = np.max(self.image[i * self.stride:i * self.stride + self.pool_size,
+                                       j * self.stride:j * self.stride + self.pool_size, d])
+        return out.reshape(1, out_height * out_width * self.depth)
+
+
+class LinearLayer:
+    """
+    从之前的BP算法中复制过来的
+    """
+
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(input_size, output_size)
+        self.threshold = np.random.randn(output_size, 1)
+        print(self.threshold)
+        self.input = None
+        self.output = None
+        self.delta = np.zeros(np.shape(self.threshold))
+        self.delta_weights = np.zeros(np.shape(self.weights))
+        self.learning_rate = 0.001
+
+    def forward(self, input):
+        self.input = input
+        self.output = 1 / (1 + np.exp(-(np.dot(input.T, self.weights) - self.threshold.T)))  # Sigmoid
+        self.output = self.output.T
+        return self.output
+
+    def backward(self, dout):
+        """
+        反向传播
+        :param dout: 上一层的输出求偏导的结果
+        :return:
+        """
+        self.delta += dout * self.output * (1 - self.output)
+        self.delta_weights += self.input * self.delta.T
+        dx = np.dot(self.weights, self.delta)
+        return dx
+
+    def update(self, learning_rate):
+        self.weights += self.delta_weights * learning_rate
+        self.threshold += self.delta * learning_rate
+        self.delta = np.zeros(np.shape(self.threshold))
+        self.delta_weights = np.zeros(np.shape(self.weights))
+        return self.weights, self.threshold
+
 
 # %% load data
 data = pd.read_csv('archive/chinese_mnist.csv')
